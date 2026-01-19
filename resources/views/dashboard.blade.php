42a6cd3c24a1879854ec4643f5fd4f56c69e634f
Nurmuhammad <nursherzil@gmail.com>
2026-01-19 21:58:55 +0500

    demo ui

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,23 +1,8 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <button>
-                        <a href="{{ route('service-requests.index') }}">Service kerak</a>
-                    </button>
-                    <br>
-                    <button>
-                        <a href="{{ route('provider-requests.index') }}">Provider Bo'lish</a>
-                    </button>
+            <div class="bg-[#FDFDFC] dark:bg-[#0a0a0a] overflow-hidden shadow-sm sm:rounded-lg">
 
-                </div>
             </div>
         </div>
     </div>
